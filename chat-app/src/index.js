8f48ab88e7f40160a7bc23c1a97d455498619a63
PhanNguyenLich <phanlich408@gmail.com>
2024-10-08 22:29:56 +0700

    fix navbar

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
---
+++ chat-app/src/pages/dashboard/Chats.js
@@ -0,0 +1,104 @@
+import {
+  alpha,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  InputBase,
+  Stack,
+  styled,
+  Typography,
+} from "@mui/material";
+import { ArchiveBox, CircleDashed, MagnifyingGlass } from "phosphor-react";
+import React from "react";
+
+const ChatElement = () => {
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 60,
+        borderRadius: 1,
+        backgroundColor: "#fff",
+      }}
+    ></Box>
+  );
+};
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.background.paper, 1),
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    width: "100%",
+  },
+}));
+
+const Chats = () => {
+  return (
+    <Box
+      sx={{
+        position: "relative",
+        height: "100vh",
+        width: 320,
+        backgroundColor: "#F8FAFF",
+        boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+      }}
+    >
+      <Stack p={3} spacing={2}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+        >
+          <Typography variant="h5">Chats</Typography>
+          <IconButton>
+            <CircleDashed />
+          </IconButton>
+        </Stack>
+        <Stack sx={{ width: "100%" }}>
+          <Search>
+            <SearchIconWrapper>
+              <MagnifyingGlass color="#709CE6" />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search..."
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+        </Stack>
+        <Stack spacing={1}>
+          <Stack direction={"row"} alignItems={"center"} spacing={1.5}>
+            <ArchiveBox size={24} />
+            <Button>Archived</Button>
+          </Stack>
+          <Divider />
+        </Stack>
+        <Stack direction={"column"}>
+          <ChatElement />
+        </Stack>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Chats;
