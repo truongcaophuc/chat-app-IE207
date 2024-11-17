d278110acdd98e12a91aaa028216951fbe5b9ae0
truongcaophuc <phuctruong.310103@gmail.com>
2024-11-17 15:54:33 +0700

    update

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -3,7 +3,14 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 
 // routes
 import { PATH_DASHBOARD } from "./routes/paths";
-export const BASE_URL = "https://localhost:3001";
+
+export const BASE_URL = "https://api.chat.codingmonk.in/";
+
+export const S3_BUCKET_NAME = 'codingmonk';
+export const AWS_ACCESS_KEY = 'AKIARPJQ4HSYLBIK2TDE';
+export const AWS_SECRET_ACCESS_KEY = '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';
+export const AWS_S3_REGION = 'ap-south-1'; // eg. ap-south-1
+
 export const defaultSettings = {
   themeMode: "light",
   themeDirection: "ltr",
--- Frontend/src/sections/Dashboard/settings/ProfileForm.js
+++ Frontend/src/sections/Dashboard/settings/ProfileForm.js
@@ -1,32 +1,31 @@
 import React, { useCallback, useState } from "react";
 import * as Yup from "yup";
+// form
 import { useForm } from "react-hook-form";
-import { Link as RouterLink } from "react-router-dom";
-
 import { yupResolver } from "@hookform/resolvers/yup";
-import {
-  Alert,
-  Button,
-  IconButton,
-  InputAdornment,
-  Link,
-  Stack,
-} from "@mui/material";
-import FormProvider, { RHFTextField } from "../../../components/hook-form";
-import { Eye, EyeSlash } from "phosphor-react";
+import FormProvider from "../../../components/hook-form/FormProvider";
+import { RHFTextField, RHFUploadAvatar } from "../../../components/hook-form";
+import { Stack } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { useDispatch, useSelector } from "react-redux";
+import { UpdateUserProfile } from "../../../redux/slices/app";
+import { AWS_S3_REGION, S3_BUCKET_NAME } from "../../../config";
 
 const ProfileForm = () => {
-  const [showPassword, setShowPassword] = useState(false);
+  const dispatch = useDispatch();
+  const [file, setFile] = useState();
+  const { user } = useSelector((state) => state.app);
 
   const ProfileSchema = Yup.object().shape({
-    name: Yup.string().required("Name is required"),
+    firstName: Yup.string().required("Name is required"),
     about: Yup.string().required("About is required"),
-    avatarUrl: Yup.string().required("Avatar is required").nullable(true),
+    avatar: Yup.string().required("Avatar is required").nullable(true),
   });
 
   const defaultValues = {
-    name: "",
-    about: "",
+    firstName: user?.firstName,
+    about: user?.about,
+    avatar: `https://${S3_BUCKET_NAME}.s3.${AWS_S3_REGION}.amazonaws.com/${user?.avatar}`,
   };
 
   const methods = useForm({
@@ -39,63 +38,67 @@ const ProfileForm = () => {
     watch,
     control,
     setValue,
-    setError,
     handleSubmit,
-    formState: { errors, isSubmitting, isSubmitSuccessful },
+    formState: { isSubmitting, isSubmitSuccessful },
   } = methods;
 
   const values = watch();
+
+  const onSubmit = async (data) => {
+    try {
+      //   Send API request
+      console.log("DATA", data);
+      dispatch(
+        UpdateUserProfile({
+          firstName: data?.firstName,
+          about: data?.about,
+          avatar: file,
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleDrop = useCallback(
     (acceptedFiles) => {
       const file = acceptedFiles[0];
 
-      //   setFile(file);
+      setFile(file);
 
       const newFile = Object.assign(file, {
         preview: URL.createObjectURL(file),
       });
 
       if (file) {
-        setValue("avatarUrl", newFile, { shouldValidate: true });
+        setValue("avatar", newFile, { shouldValidate: true });
       }
     },
     [setValue]
   );
 
-  const onSubmit = async (data) => {
-    try {
-      // submit data to backend
-      console.log("data", data);
-    } catch (error) {
-      console.log(error);
-      reset();
-      setError("afterSubmit", {
-        ...error,
-        message: error.message,
-      });
-    }
-  };
-
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={3}>
-        <Stack spacing={3}>
-          {!!errors.afterSubmit && (
-            <Alert security="error">{errors.afterSubmit.message}</Alert>
-          )}
+      <Stack spacing={4}>
+        <RHFUploadAvatar name="avatar" maxSize={3145728} onDrop={handleDrop} />
 
-          <RHFTextField
-            helperText={"This name is visible to your contacts"}
-            name="firstName"
-            label="First Name"
-          />
-          <RHFTextField multiline rows={4} name="about" label="About" />
-        </Stack>
-        <Stack direction={"row"} justifyContent={"end"}>
-          <Button color="primary" size="large" variant="outlined">
+        <RHFTextField
+          helperText={"This name is visible to your contacts"}
+          name="firstName"
+          label="First Name"
+        />
+        <RHFTextField multiline rows={4} name="about" label="About" />
+
+        <Stack direction={"row"} justifyContent="end">
+          <LoadingButton
+            color="primary"
+            size="large"
+            type="submit"
+            variant="contained"
+            // loading={isSubmitSuccessful || isSubmitting}
+          >
             Save
-          </Button>
+          </LoadingButton>
         </Stack>
       </Stack>
     </FormProvider>
