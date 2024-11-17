244384b1cceb0e67c942c752e310791585038aa1
truongcaophuc <phuctruong.310103@gmail.com>
2024-11-17 13:43:14 +0700

    integrating register/signup api

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -3,11 +3,12 @@ import { Navigate, useRoutes } from "react-router-dom";
 
 // layouts
 import DashboardLayout from "../layouts/dashboard";
+import AuthLayout from "../layouts/auth";
 
 // config
 import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
-import MainLayout from "../layouts/main";
+
 const Loadable = (Component) => (props) => {
   return (
     <Suspense fallback={<LoadingScreen />}>
@@ -20,12 +21,13 @@ export default function Router() {
   return useRoutes([
     {
       path: "/auth",
-      element: <MainLayout />,
+      element: <AuthLayout />,
       children: [
-        { element: <LoginPage />, path: "login" },
-        { element: <RegisterPage />, path: "register" },
-        { element: <ResetPasswordPage />, path: "reset-password" },
-        { element: <NewPasswordPage />, path: "new-password" },
+        { path: "login", element: <LoginPage /> },
+        { path: "register", element: <RegisterPage /> },
+        { path: "reset-password", element: <ResetPasswordPage /> },
+        { path: "new-password", element: <NewPasswordPage /> },
+        {path: "verify", element: <VerifyPage /> },
       ],
     },
     {
@@ -34,15 +36,20 @@ export default function Router() {
       children: [
         { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
         { path: "app", element: <GeneralApp /> },
+        { path: "group", element: <Group /> },
         { path: "settings", element: <Settings /> },
-        { path: "group", element: <GroupPage /> },
-        { path: "call", element: <CallPage /> },
-        { path: "profile", element: <ProfilePage /> },
+        { path: "conversation", element: <Conversation /> },
+        { path: "chats", element: <Chats /> },
+        { path: "contact", element: <Contact /> },
+        { path: "profile", element: <Profile /> },
 
+        {path: "call", element: <CallPage />},
+        
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
       ],
     },
+
     { path: "*", element: <Navigate to="/404" replace /> },
   ]);
 }
@@ -50,25 +57,27 @@ export default function Router() {
 const GeneralApp = Loadable(
   lazy(() => import("../pages/dashboard/GeneralApp"))
 );
+const Conversation = Loadable(
+  lazy(() => import("../pages/dashboard/Conversation"))
+);
+const Chats = Loadable(lazy(() => import("../pages/dashboard/Chats")));
+const Group = Loadable(lazy(() => import("../pages/dashboard/Group")));
+const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
+const Contact = Loadable(lazy(() => import("../sections/dashboard/Contact")));
+const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 
 const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
-
+const VerifyPage = Loadable(lazy(() => import("../pages/auth/Verify")));
 const RegisterPage = Loadable(lazy(() => import("../pages/auth/Register")));
-
 const ResetPasswordPage = Loadable(
   lazy(() => import("../pages/auth/ResetPassword"))
 );
-
 const NewPasswordPage = Loadable(
   lazy(() => import("../pages/auth/NewPassword"))
 );
 
+// Settings
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
-
-const CallPage = Loadable(lazy(() => import("../pages/dashboard/Call")));
-
-const GroupPage = Loadable(lazy(() => import("../pages/dashboard/Group")));
-
-const ProfilePage = Loadable(lazy(() => import("../pages/dashboard/Profile")));
-
-const Page404 = Loadable(lazy(() => import("../pages/Page404")));
+const Profile = Loadable(
+  lazy(() => import("../pages/dashboard/Settings/Profile"))
+);
